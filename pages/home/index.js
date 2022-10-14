7186cc24eed2cebc675a0cc6e9f1b15fd7cdb4fd
Gabriel <gabpavao@outlook.com>
2022-10-14 20:10:03 -0300

    Inserting API in the Header of the Profile Page

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,36 @@
-/* Desenvolva sua lógica aqui...*/
+const inputSearchUser = document.querySelector('#user')
+const buttonSubmit    = document.querySelector('#submit')
+
+function getInputValue(input, button) {
+    button.addEventListener('click', async (event) => {
+        event.preventDefault()
+        try {
+            
+            button.innerText ='Carregando...'
+
+            let body = document.body
+            body.style.cursor = 'progress'
+            
+            let response = await fetch(`https://api.github.com/users/${input.value}`)
+
+            if (response.status != 200) {
+                throw new Error('Usuário não encontrado')
+            }
+
+            let user = await response.json()
+            localStorage.setItem('userFound', JSON.stringify(user))
+
+            
+            window.location.assign("http://127.0.0.1:5501/pages/profile/index.html")
+            
+        } catch (error) {
+            event.preventDefault()
+            alert(error.message) 
+            button.innerText ='Ver perfil do github'
+        }
+    })
+    
+}
+
+getInputValue(inputSearchUser, buttonSubmit)
+
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,16 @@
-/* Desenvolva sua lógica aqui...*/
+const userData = JSON.parse(localStorage.getItem('userFound'))
+console.log(userData)
+
+
+function insertDataHeader(obj){
+    let imgProfile    = document.querySelector('#profileImg')
+    let h2ProfileName = document.querySelector('#profileName')
+    let pJob          = document.querySelector('#profileJob')
+    let aLinkEmail    = document.querySelector('#profileEmail')
+    
+    imgProfile.src = obj.avatar_url
+    h2ProfileName.innerText = obj.name
+    pJob.innerText = obj.bio
+    aLinkEmail.setAttribute('href', `mailto:${obj.email}`)
+}
+insertDataHeader(userData)
