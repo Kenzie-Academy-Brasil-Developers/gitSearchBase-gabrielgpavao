d087fe92f48c2055968840b12e484c1e9b989279
Gabriel <gabpavao@outlook.com>
2022-10-14 21:07:11 -0300

    Repository List

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,5 +1,5 @@
 const userData = JSON.parse(localStorage.getItem('userFound'))
-console.log(userData)
+console.log(userData.repos_url)
 
 
 function insertDataHeader(obj){
@@ -14,3 +14,44 @@ function insertDataHeader(obj){
     aLinkEmail.setAttribute('href', `mailto:${obj.email}`)
 }
 insertDataHeader(userData)
+
+
+
+const ulRepositoryList = document.querySelector('.repository-list')
+
+function createRepositoryCard (currentRep) {
+    let liRepositoryCard       = document.createElement('li')
+    let h3RepositoryName       = document.createElement('h3')
+    let pRepositoryDescription = document.createElement('p')
+    let divButtons             = document.createElement('div')
+    let buttonRepository       = document.createElement('button')
+    let buttonDemo             = document.createElement('button')
+
+
+    liRepositoryCard.className       = 'repository-card'
+    h3RepositoryName.innerText       = `${currentRep.name}`
+    pRepositoryDescription.innerText = `${currentRep.description}`
+    buttonRepository.className       = 'repository-button'
+    buttonRepository.innerText       = 'Repositório'
+    buttonRepository.addEventListener('click', () => {
+        window.location.assign(`${currentRep.html_url}`)
+    })
+
+    buttonDemo.className             = 'demo'
+    buttonDemo.innerText             = 'Demo'
+
+
+    divButtons.append(buttonRepository, buttonDemo)
+    liRepositoryCard.append(h3RepositoryName, pRepositoryDescription, divButtons)
+    ulRepositoryList.append(liRepositoryCard)
+        
+}
+
+
+async function insertDataRepository (obj) {
+    let repositories = await (await fetch(obj.repos_url)).json()
+    repositories.forEach(element => {
+        createRepositoryCard(element)
+    });
+}
+insertDataRepository(userData)
