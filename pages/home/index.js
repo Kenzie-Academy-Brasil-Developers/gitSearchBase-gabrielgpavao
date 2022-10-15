a461167394d4257f8feaa6bc7671f878d71533b8
Gabriel <gabpavao@outlook.com>
2022-10-15 14:08:58 -0300

    User Not-Found Applications

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,14 +1,17 @@
+const body            = document.querySelector('body')
 const inputSearchUser = document.querySelector('#user')
 const buttonSubmit    = document.querySelector('#submit')
+const spanError       = document.querySelector('.not-found')
+
+
+let recentUsers = [JSON.parse(localStorage.getItem('recentUsers'))]
+console.log(recentUsers)
 
 function getInputValue(input, button) {
     button.addEventListener('click', async (event) => {
         event.preventDefault()
         try {
-            
             button.innerText ='Carregando...'
-
-            let body = document.body
             body.style.cursor = 'progress'
             
             let response = await fetch(`https://api.github.com/users/${input.value}`)
@@ -18,19 +21,25 @@ function getInputValue(input, button) {
             }
 
             let user = await response.json()
+
             localStorage.setItem('userFound', JSON.stringify(user))
 
             
+            // recentUsers.splice(2, 1)
+            // recentUsers.unshift(user)
+            // localStorage.setItem('recentUsers', JSON.stringify(recentUsers))
+            // console.log(recentUsers)
+
             window.location.assign("http://127.0.0.1:5501/pages/profile/index.html")
             
-        } catch (error) {
-            event.preventDefault()
-            alert(error.message) 
+        } catch(error) {
             button.innerText ='Ver perfil do github'
+            body.style.cursor = 'auto'
+            spanError.style.display = 'block'
+            spanError.innerText = `${error.message}`
         }
     })
     
 }
 
 getInputValue(inputSearchUser, buttonSubmit)
-
